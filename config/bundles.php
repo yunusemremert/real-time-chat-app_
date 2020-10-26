ffcceccf8faebe4074a8da3ffe547a3f1a2a4524
Yunus Emre Mert <yunusemremert@yandex.com>
2020-10-26 23:53:58 +0300

    maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
